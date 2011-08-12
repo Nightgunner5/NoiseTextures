bae52dcfd5b4e5817045513479d1e857c3475846
Ben L <nightgunner5@llamaslayers.net>
2011-08-12 12:06:29 -0500

    Add grass texture generator Add examples Add timing

--- src/main/java/net/llamaslayers/noisetextures/GrassTextureGenerator.java
+++ src/main/java/net/llamaslayers/noisetextures/GrassTextureGenerator.java
@@ -1,27 +1,59 @@
 package net.llamaslayers.noisetextures;
 
+import java.util.Random;
 import org.bukkit.util.noise.SimplexOctaveGenerator;
 
 public class GrassTextureGenerator extends AbstractTextureGenerator {
-	//private final SimplexOctaveGenerator light;
-	//private final SimplexOctaveGenerator medium;
-	//private final SimplexOctaveGenerator heavy;
+	private final SimplexOctaveGenerator density;
+	private final SimplexOctaveGenerator light;
+	private final SimplexOctaveGenerator medium;
+	private final SimplexOctaveGenerator heavy;
 
 	public GrassTextureGenerator(long seed) {
 		super(seed);
+
+		Random r = getSeed();
+		density = getNoise(r, 3);
+		density.setScale(1.0 / 256);
+		light = getNoise(r, 8);
+		light.setScale(1.0 / 64);
+		medium = getNoise(r, 8);
+		light.setScale(1.0 / 16);
+		heavy = getNoise(r, 8);
+		light.setScale(1.0 / 4);
 	}
 
 	@Override
 	public byte getTone(double x, double y) {
-		throw new UnsupportedOperationException("Not supported yet.");
+		return (byte) Math.min(Math.max(lerp3(light.noise(x, y, 0.5, 0.5, true) / 4,
+											  medium.noise(x, y, 0.5, 0.5, true) / 2,
+											  heavy.noise(x, y, 0.5, 0.5, true) / 1.5,
+											  density.noise(x, y, 0.5, 0.5, true))
+				* 128 + 128, 0), 255);
 	}
 
 	@Override
 	protected byte[] generate(int x, int y, int width, int height, int step) {
-		//byte[] b = new byte[width * height * step * step];
+		byte[] b = new byte[width * height * step * step];
 
-		throw new UnsupportedOperationException("Not supported yet.");
+		int i = 0;
+		for (double _x = x; _x < x + width; _x += 1.0 / step) {
+			for (double _y = y; _y < y + height; _y += 1.0 / step) {
+				b[i] = (byte) Math.min(Math.max(lerp3(light.noise(_x, _y, 0.5, 0.5, true) / 4,
+													  medium.noise(_x, _y, 0.5, 0.5, true) / 2,
+													  heavy.noise(_x, _y, 0.5, 0.5, true) / 1.5,
+													  density.noise(_x, _y, 0.5, 0.5, true))
+						* 128 + 128, 0), 255);
+				i++;
+			}
+		}
 
-		//return b;
+		return b;
+	}
+
+	private double lerp3(double a, double b, double c, double variant) {
+		if (variant < 0)
+			return -a * variant + b * (1 + variant);
+		return b * variant + c * (1 - variant);
 	}
 }
--- src/main/java/net/llamaslayers/noisetextures/Main.java
+++ src/main/java/net/llamaslayers/noisetextures/Main.java
@@ -11,28 +11,70 @@ public class Main {
 		AbstractTextureGenerator gen;
 		BufferedImage img;
 
+		long start = 0;
+		long end = 0;
+
+		start = System.nanoTime();
 		gen = new TileTextureGenerator(0);
+		end = System.nanoTime();
+		System.out.println("Construction of TileTextureGenerator with seed 0 took " + ((end - start) / 1000000000.0) + " seconds.");
+		start = System.nanoTime();
 		img = gen.render(0, 0, 512, 512, 4, Color.GRAY);
 		ImageIO.write(img, "png", new File("tile-0-gray.png"));
+		end = System.nanoTime();
+		System.out.println("Render of tile-0-gray.png took " + ((end - start) / 1000000000.0) + " seconds.");
+		start = System.nanoTime();
 		img = gen.render(0, 0, 512, 512, 4, new Color(0x9DCBE3));
 		ImageIO.write(img, "png", new File("tile-0-blue.png"));
+		end = System.nanoTime();
+		System.out.println("Render of tile-0-blue.png took " + ((end - start) / 1000000000.0) + " seconds.");
+		start = System.nanoTime();
 		gen = new TileTextureGenerator(1);
+		end = System.nanoTime();
+		System.out.println("Construction of TileTextureGenerator with seed 1 took " + ((end - start) / 1000000000.0) + " seconds.");
+		start = System.nanoTime();
 		img = gen.render(0, 0, 512, 512, 4, Color.GRAY);
 		ImageIO.write(img, "png", new File("tile-1-gray.png"));
+		end = System.nanoTime();
+		System.out.println("Render of tile-1-gray.png took " + ((end - start) / 1000000000.0) + " seconds.");
 
+		start = System.nanoTime();
 		gen = new DirtTextureGenerator(0);
+		end = System.nanoTime();
+		System.out.println("Construction of DirtTextureGenerator with seed 0 took " + ((end - start) / 1000000000.0) + " seconds.");
+		start = System.nanoTime();
 		img = gen.render(0, 0, 512, 512, 4, new Color(0x5E3C00));
 		ImageIO.write(img, "png", new File("dirt-0-wet.png"));
+		end = System.nanoTime();
+		System.out.println("Render of dirt-0-wet.png took " + ((end - start) / 1000000000.0) + " seconds.");
+		start = System.nanoTime();
 		gen = new DirtTextureGenerator(1);
+		end = System.nanoTime();
+		System.out.println("Construction of DirtTextureGenerator with seed 1 took " + ((end - start) / 1000000000.0) + " seconds.");
+		start = System.nanoTime();
 		img = gen.render(0, 0, 512, 512, 4, new Color(0x96886E));
 		ImageIO.write(img, "png", new File("dirt-1-dry.png"));
+		end = System.nanoTime();
+		System.out.println("Render of dirt-1-dry.png took " + ((end - start) / 1000000000.0) + " seconds.");
 
+		start = System.nanoTime();
 		gen = new IntestinesTextureGenerator(0);
+		end = System.nanoTime();
+		System.out.println("Construction of IntestinesTextureGenerator with seed 0 took " + ((end - start) / 1000000000.0) + " seconds.");
+		start = System.nanoTime();
 		img = gen.render(0, 0, 512, 512, 4, new Color(0xEF3728));
 		ImageIO.write(img, "png", new File("intestines-0-eww.png"));
+		end = System.nanoTime();
+		System.out.println("Render of intestines-0-eww.png took " + ((end - start) / 1000000000.0) + " seconds.");
 
-		//gen = new GrassTextureGenerator(0);
-		//img = gen.render(0, 0, 512, 512, 4, new Color(0x70CC25));
-		//ImageIO.write(img, "png", new File("grass-0-green.png"));
+		start = System.nanoTime();
+		gen = new GrassTextureGenerator(0);
+		end = System.nanoTime();
+		System.out.println("Construction of GrassTextureGenerator with seed 0 took " + ((end - start) / 1000000000.0) + " seconds.");
+		start = System.nanoTime();
+		img = gen.render(0, 0, 512, 512, 4, new Color(0x60AA25));
+		ImageIO.write(img, "png", new File("grass-0-green.png"));
+		end = System.nanoTime();
+		System.out.println("Render of grass-0-green.png took " + ((end - start) / 1000000000.0) + " seconds.");
 	}
 }
